38a241d858c2ef52e60071616f0c05f137724736
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2018-10-08 00:18:29 -0700

    Added MongoDB and Student model

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-// Because we willm want to execute HTTP requests,
-// we are going to install express into our program
-// and create an instance called "app"
+// Installing express, using const app as instantiation
 const express = require("express")
 const app = express()
-// We are going to be redneering HTML templates, so
-// we will install handlebars
+
+// Installing handlebars
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-//Let us create a mock array of students
-let students = [
-  { name: "Adam", bio: "Loves to code"},
-  { name: "Billy", bio: "Loves to cook"}
-]
+// Installing mongoose and mongod
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/contractor')
+
+const Student = mongoose.model("Student", {
+  name: String,
+  bio: String
+});
+
+// // Mock array of students (if not connected to DB)
+// let students = [
+//   { name: "Adam", bio: "Loves to code"},
+//   { name: "Billy", bio: "Loves to cook"}
+// ]
 
 //Express methods
 app.get('/', (req, res) => {
-  res.render("home", { msg: "Handlebars are Cool!" });
+  res.render("home", { msg: "Welcome to Scholarship Flight!" });
 })
 
 // Action: Index
 app.get("/students", (req, res) => {
-  res.render("students-index", { students: students});
+  Student.find()
+    .then(students => {
+        res.render("students-index", { students: students});
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.listen(3000, () => {
